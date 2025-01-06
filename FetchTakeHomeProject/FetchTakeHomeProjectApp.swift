ef7971eb60e32b36fda41a3e5c25515722b64533
Jerry <59536381+Jerryc95@users.noreply.github.com>
2025-01-06 18:59:45 -0500

    Initial Commit

---
+++ FetchTakeHomeProject/FetchTakeHomeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchTakeHomeProjectApp.swift
+//  FetchTakeHomeProject
+//
+//  Created by Jerry Cox on 1/6/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchTakeHomeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
